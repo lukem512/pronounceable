e5e2db825882d404e0318328a643e11ff77aead5
lukem512 <lukem95@gmail.com>
2016-04-03 16:07:25 +0100

    Added examples

---
+++ examples/score.js
@@ -0,0 +1,14 @@
+// Luke Mitchell, 2016
+// https://github.com/lukem512/pronounceable
+
+var pronounceable = require('pronounceable');
+
+// Scoring a word using the standard dataset.
+// The output is a normalised score. The higher the number
+// the more pronounceable the word.
+
+var morePronouncable = 'peonies';
+console.log(morePronouncable, pronounceable.score(morePronouncable));
+
+var lessPronouncable = 'sshh';
+console.log(lessPronouncable, pronounceable.score(lessPronouncable));
---
+++ examples/test.js
@@ -0,0 +1,14 @@
+// Luke Mitchell, 2016
+// https://github.com/lukem512/pronounceable
+
+var pronounceable = require('pronounceable');
+
+// Testing a word using the standard dataset.
+// The `test` method returns true is a word is pronouncable
+// and false otherwise.
+
+var yes = 'samosa';
+console.log(yes, pronounceable.test(yes));
+
+var no = 'xghsii';
+console.log(no, pronounceable.test(no));
